0576e8c8bacff2301a2c986ccfd4c6abab434637
Rebecca Eilering <rebeccaeilering@gmail.com>
2019-08-25 14:58:23 -0500

    added basic list item styling and moved links, added font

--- public/results/results.js
+++ public/results/results.js
@@ -5,11 +5,11 @@ getData();
 
       for (item of data) {
         const root = document.createElement('div');
-        const figurine = document.createElement('div');
-        const color = document.createElement('div');
-        const location = document.createElement('div');
-        const city = document.createElement('div');
-        const date = document.createElement('div');
+        const figurine = document.createElement('span');
+        const color = document.createElement('span');
+        const location = document.createElement('span');
+        const city = document.createElement('span');
+        const date = document.createElement('span');
         const image = document.createElement('img');
 
         root.classList.add('list-item');
@@ -23,7 +23,7 @@ getData();
         location.textContent = `Location: ${item.location}`;;
         city.textContent = `City: ${item.city}`;;
         const dateString = new Date(item.timestamp).toLocaleString();
-        date.textContent = `Date: ${dateString}`;
+        date.textContent = `Date Uploaded: ${dateString}`;
         image.src = item.image64;
 
         root.append(figurine, color, location, city, date, image);
